3a4aa1842c66edd5cd978d25627b2edf24d9b679
Aymen <aymenjawad55@gmail.com>
2023-09-08 10:24:09 -0700

    C 0x00 8.0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - output point
+ *
+ * Description: this is a artsy program that uses printf to be artsy
+ *
+ * Return: 1 (error)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	       return 1;	
+}
