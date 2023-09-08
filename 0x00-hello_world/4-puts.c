4604752927861aa7d5e3aef65bb7478036f9ae5d
Aymen <aymenjawad55@gmail.com>
2023-09-07 18:28:00 -0700

    C 0x00 4.4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * This is the main function.
+ *
+ * it prints a cool message
+ *
+ * @return 0 indicating successful execution.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
