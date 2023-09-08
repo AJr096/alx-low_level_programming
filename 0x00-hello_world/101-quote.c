3caf73d9737315ed691a39db6db112105cdac220
Aymen <aymenjawad55@gmail.com>
2023-09-08 10:29:09 -0700

    C 0x00 8.1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  *
  * Description: this is a artsy program that uses printf to be artsy
  *
- * Return: 1 (error)
+ * Return: (1) means error
  */
 
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	       return 1;	
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
