6d3d18f2a9e88d5a420cb23fad0a8b5b826d7c59
Aymen <aymenjawad55@gmail.com>
2023-09-07 18:45:56 -0700

    0x00 5.0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: a C program that prints with the printf funct
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\,");
+	return (0);
+}
