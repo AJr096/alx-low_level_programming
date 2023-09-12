d32a039a31b0c3bdf454607cb5160cfc57b3641e
Aymen <aymenjawad55@gmail.com>
2023-09-11 18:50:42 -0700

    C 0x01 2.0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - ENTRY PLEASE
+ *
+ * Description: a to z
+ *
+ * Return: 0 means success
+ */
+
+int main(void)
+{
+	chat alphab = 'a'
+
+	while (alphab <= 'z')
+
+	{
+		putchar(alphab);
+		alphab++;
+		putchar('\n')
+	}
+
+	return (0)
+}
