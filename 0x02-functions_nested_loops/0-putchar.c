7b907a3a80183d375728360ebc6a5c6fe1e706e0
Aymen <aymenjawad55@gmail.com>
2023-09-12 15:38:37 -0700

    C 0x02 0.0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program taht prints using printf
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
