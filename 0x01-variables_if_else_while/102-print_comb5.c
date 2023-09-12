a217ee12145b1c37eb7040c70beffaabc22c91cd
Aymen <aymenjawad55@gmail.com>
2023-09-12 11:17:08 -0700

    C 0x01 12.0

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - point of entry
+ *
+ * Description: all possible combinations of two digit numb
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int numb1 = 0, numb2;
+
+	while (numb1 <= 99)
+	{
+		numb2 = numb2;
+		while (numb2 <= 99)
+		{
+			if (numb1 != numb2)
+			{
+				putchar((numb1 / 10) + 48);
+				putchar((numb1 % 10) + 48);
+				putchar(' ');
+				putchar((numb2 / 10) + 48);
+				putchar((numb2 % 10) + 48);
+
+				if (numb1 != 98 || numb2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			numb2++;
+		}
+		numb1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
