ad4d86aff047799bbe7cfa3450586f0f220de2db
Aymen <aymenjawad55@gmail.com>
2023-09-13 18:14:30 -0700

    C 0x02 3.0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - is a function taht checks if char. is low case
+ *
+ * @c: checks input of the function
+ *
+ * Return: rturns 1 if c is lowcase otherwise gives 0 as success
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
+	
