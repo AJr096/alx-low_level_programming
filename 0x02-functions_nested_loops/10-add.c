4181c5b5e6008f155d62f6cd8fc3516d54a4c839
Aymen <aymenjawad55@gmail.com>
2023-09-13 19:06:37 -0700

    C 0x02 10.0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - marriage
+ *
+ * @mann: first integer
+ * @frau: second integer
+ *
+ * Return: kindern
+ */
+
+int add(int mann, int frau)
+{
+	int kindern = mann + frau;
+
+	return (kindern);
+}
