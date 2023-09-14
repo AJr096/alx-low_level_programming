8235bfd3ca9e9f4206e8e1d88a0e457db56244ad
Aymen <aymenjawad55@gmail.com>
2023-09-13 18:20:19 -0700

    C 0x02 4.0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - a function taht checks if a letter is low or up case
+ *
+ * @c) takes input from anderes fonctionen
+ *
+ * Return: 1 is truth else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c <= 90 && c >= 65)
+		return (1);
+	return (0);
+}
