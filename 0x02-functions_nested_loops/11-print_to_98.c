6d9a4c8fcb905b1e52ec7bc2d957c473c079cb33
Aymen <aymenjawad55@gmail.com>
2023-09-13 19:14:11 -0700

    C 0x02 11.0

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all naturals using printf
+ *
+ * @n: integer input
+ *
+ * Return: void
+ */
+
+void pront_to_98(int n)
+{
+	int c;
+
+	if (n > 98)
+		for (c = n; c > 98; c--)
+			printf("%d, ", c);
+	else
+		for (c = n; c < 98; count++)
+			printf("%d, ", c);
+	printf("98\n");
+}
