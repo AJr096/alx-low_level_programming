420abaefc92334628569260ff04af6c10a2d3f2c
Aymen <aymenjawad55@gmail.com>
2023-09-13 17:54:49 -0700

    C 0x02 1.00

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry
+ *
+ * Description: using the function _putchar to print a-z
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	_putchar('\n')
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
+/* task 0*/
 int _putchar(char c);
+/* task 1*/
+void print_alphabet(void);
 
 #endif
