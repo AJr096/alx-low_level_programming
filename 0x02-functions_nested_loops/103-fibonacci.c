be4557216d21532f86f919a6e80bfb978aed2ad2
Aymen <aymenjawad55@gmail.com>
2023-09-14 10:37:14 -0700

    C 0x02 15.0

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry bitte
+ *
+ * Description: finds and prints the sum of the even-valued terms under 4 mil
+ *
+ * Return: 0 on Erfolg
+ */
+
+int main(void)
+{
+	unsigned long erste = 1, summe = 0, zweite = 2;
+
+	while (zweite <= 4000000)
+	{
+		{
+		if (zweite % 2 == 0)
+			summe += zweite;
+		}
+
+	unsigned long nächste = erste + zweite;
+	erste = zweite;
+	zweite = nächste;
+	}
+
+	printf("%lu\n", summe);
+
+	return (0);
+}
+
