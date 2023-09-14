0ba7e59b37d6c7d50fea188fbc0784089fe72f31
Aymen <aymenjawad55@gmail.com>
2023-09-13 18:34:07 -0700

    C 0x02 6.2

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _abs.c - accentute sthe positive and elimenates teh negative
+ * _abs - accentute sthe positive and elimenates teh negative
  *
  *  @,: takes integer type as input for funct
  *
  *  Return: always 0 succ
  */
 
-int _abs.c(int n)
+int _abs(int n)
 {
 	if (n < 0)
 		n = (-1) * n;
