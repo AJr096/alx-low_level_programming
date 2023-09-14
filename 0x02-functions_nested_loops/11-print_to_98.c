3eb04d6f3ace6476d60d71ffdf9dc7ac15f319af
Aymen <aymenjawad55@gmail.com>
2023-09-13 19:14:56 -0700

    C 0x02 11.1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void pront_to_98(int n)
 		for (c = n; c > 98; c--)
 			printf("%d, ", c);
 	else
-		for (c = n; c < 98; count++)
+		for (c = n; c < 98; c++)
 			printf("%d, ", c);
 	printf("98\n");
 }
