7ea93e647994b91907bdb6d7145e40cb75517cca
Aymen <aymenjawad55@gmail.com>
2023-09-14 09:53:37 -0700

    C 0x02 13.3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,17 +8,16 @@
  * Return: 0 success
  */
 
-int main(void) 
+int main(void)
 {
 	int i, sum;
-	{
-	for (i = 1; i < 1024; i++)
-		if (i % 3 == 0 || i % 5 == 0)
-		       sum += i;	
-	{
-		printf("%d\n", sum);
-	}
-    }
 
-    return (0);
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
