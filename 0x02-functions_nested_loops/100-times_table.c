9ffcec6568ab3688671cbb5701ddf0b5b326d972
Aymen <aymenjawad55@gmail.com>
2023-09-14 09:15:28 -0700

    C 0x02 12.0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints time table of n
+ *
+ * @n: takes integer input
+ */
+
+void print_times table(int n)
+{
+	int r, m, b;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (b = 0; b <= n; n++)
+		{
+			_putchar(48);
+			for (m = 1; m <= n; m++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				p = b * m;
+
+				if (p <= 9)
+					_putchar(' ');
+				if (p <= 99)
+					_putchar(' ');
+				if (p >= 100)
+				{
+					_putchar((p / 100) + 48);
+					_putchar((p / 10) % 10 + 48);
+				}
+				else if (p <= 99 && p >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((p % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,23 +7,24 @@ int _putchar(char c);
 void print_alphabet(void);
 /* task2 */
 void print_alphabet_x10(void);
-/**/
+/* task3 */
 int _islower(int c);
-/**/
+/* task4 */
 int _isalpha(int c);
-/**/
+/* task5 */
 int print_sign(int n);
-/**/
+/* task6 */
 int _abs(int);
-/**/
+/* task7 */
 int print_last_digit(int);
-/**/
+/* task8 */
 void jack_bauer(void);
-/**/
+/* task9 */
 void times_table(void);
-/**/
+/* task10 */
 int add(int, int);
-/**/
+/* task11 */
 void print_to_98(int n);
-
+/* task12 */
+void print_times_table(int n);
 #endif
