257b996bcde0e328898105458c530d9b5e45de7a
Aymen <aymenjawad55@gmail.com>
2023-09-13 18:49:55 -0700

    C 0x02 8.1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - function that prints every minute of the day J.B., from0000 to2359
+ * jack_bauer - function that prints every minute of the day J.B., from00 to59
  *
  * Description: it is what it is
  *
@@ -16,11 +16,11 @@ void jack_bauer(void)
 	{
 		for (mn = 0; mn <= 59; mn++)
 		{
-			_putchar((hr / 10)+ 48);
-			_putchar((hr % 10)+ 48);
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar(':');
-			_putchar((mn / 10)+ 48);
-			_putchar((mn % 10)+ 48);
+			_putchar((mn / 10) + 48);
+			_putchar((mn % 10) + 48);
 			_putchar('\n');
 		}
 	}
