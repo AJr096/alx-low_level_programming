4be53fa76b3b5ab8b32159ea38d20bd52cd02a6a
Aymen <aymenjawad55@gmail.com>
2023-09-17 15:38:20 -0700

    C 0x04 4.0

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbs 0to9 except 2and4
+ *
+ * Return: 0 always
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+	}
+		_putchar('\n');
+}
