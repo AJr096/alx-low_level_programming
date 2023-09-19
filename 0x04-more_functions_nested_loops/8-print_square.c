f892dd846b9ac6ae39e8f15e87f2775a1b478271
Aymen <aymenjawad55@gmail.com>
2023-09-19 12:46:00 -0700

    C 0x04 8.0

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a squary square, a bit too long tho
+ *
+ * @size: sizeu of the squararu
+ *
+ * Return: ohne
+ */
+
+void print_square(int size)
+{
+	int ln, clm;
+
+	if (n > 0)
+	{
+		for (ln = 0; ln < size; ln++)
+		{
+			for (clm = 0; clm < size; clm++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
