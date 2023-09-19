3993ef5bdc6452ace5ceae26072311b36b5255ce
Aymen <aymenjawad55@gmail.com>
2023-09-19 13:18:03 -0700

    C 0x04 9.0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry punk
+ *
+ * Description: classic fizzinga nd buzzinga nd fizzbuzzing
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int n;
+		for (n = 1; n < 101; n++)
+		{
+			if (n % 3 == 0 && n % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (n % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else if (n % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else
+			{
+			printf("%d ", n);
+			}
+		}
+
+	printf("\n");
+
+	return (0);
+}
