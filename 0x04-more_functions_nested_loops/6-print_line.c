27ec31c0f5a669a208020bad456743424ede01e1
Aymen <aymenjawad55@gmail.com>
2023-09-19 10:40:14 -0700

    C 0x04 6.0

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include  "main.h"
+
+/**
+ * print_line - it draws line, simple
+ *
+ * @n: numbers of lines
+ *
+ * Return: void
+ */
+
+void print_line(int n);
+{
+	int lnnumb;
+
+	if (n > 0)
+	{
+		for (lnnumb = 0; lnnumb < n; lnnumb++)
+		_putchar('_');
+	_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
