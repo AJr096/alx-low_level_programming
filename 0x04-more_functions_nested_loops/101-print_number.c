327c5171fa2092db66c6af3c2ffe0edafd643584
Aymen <aymenjawad55@gmail.com>
2023-09-20 11:55:34 -0700

    C 0x04 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer, simple as that
+ *
+ * @n: the integer
+ */
+
+void print_number(int n)
+{
+	unsigned int m
+	m = n /* if yie know ye know */
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -m;
+	}
+
+	if (m / 10 >= 0)
+	{
+		print_number(m / 10); /* magic */
+	}
+
+	_putchar((m % 10) + '0'); /* or 48 doesnt matter */
+}
