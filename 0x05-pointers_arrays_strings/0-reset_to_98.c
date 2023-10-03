14170cf60602e650f7102da46bf87e9bc05a5747
Aymen <aymenjawad55@gmail.com>
2023-10-03 08:01:25 -0700

    0.1 C5

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - take a ptr to an int as prmtr and updates the value it points to to 98
+ * reset_to_98 - take a ptr to an int as prmtr upd the value it points to 98
  *
  * @n: integer parameter
  *
