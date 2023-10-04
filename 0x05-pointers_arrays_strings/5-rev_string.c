81e665c51b17a4ccdfca2b5ee206ee3921bcefd0
Aymen <aymenjawad55@gmail.com>
2023-10-04 10:57:45 -0700

    C 0x05 5.0

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** 
+ * rev_string - reveses the given string
+ *
+ * @s: string prmter input
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int length, count;
+	char step;
+
+	for (length = -1; s[length] != '\0'; length++);
+
+	for (count = 0; count < length / 2)
+	{
+		step = s[count];
+		s[count] = s[length - count];
+		s[length - count] = step;
+	}
+}
