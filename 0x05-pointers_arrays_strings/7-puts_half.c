aaf2076b7f63e60c09aac4f7c8b7baaebad7e8cc
Aymen <aymenjawad55@gmail.com>
2023-10-04 15:54:02 -0700

    C 0x05 7.0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of stng
+ *
+ * @str: char array
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != '\0'; length++)
+		;
+	length++;
+	for (length /= 2; str[length != '\0'; length++])
+	{
+		_putchar(str[length]);
+	}
+	_putchar('\n');
+}
