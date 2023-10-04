912be7d5e4eb4a428d54415cfb94e2ae93fd59dc
Aymen <aymenjawad55@gmail.com>
2023-10-04 16:02:33 -0700

    C 0x05 8.0

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prnt n elemnt of array of ints
+ *
+ * @a: string para inut
+ * @n: elemnt para input
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int count;
+
+	for (count = 0; count < n; count++)
+	{
+		if (coun != (n - 1))
+			printf("%d, ", a[count]);
+		else
+			printf("%d", a[count]);
+	}
+	printf("\n");
+}
